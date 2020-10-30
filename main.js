58fd61ce0215bfb849b3ad5f9430ea19ec24cfee
7 <7te@protonmail.ch>
2020-10-30 00:30:12 -0400

    rewrite

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 /*
-DATA FLOWWWW
+# upcoming goals
+
+add $HOME
+add config options
+
+# thoughts
+
+-- data flow should be something like --
 
 keyUp listener = done
 increment keypress variable = kinda done, need to look at node keyboard interface
@@ -8,6 +15,20 @@ start new typing object thingy
 calculate cpm based on typing thing
 translate cpm to wpm (/5 lol)
 render wpm 
+
+-- other thoughts --
+
+Okay so i need a self invoked function here to just listen to all key events when the application is running?
+
+# unanswered questions
+
+how does running a node app as a background process work?
+
+look up how to add progress bar type thing as it runs
+
+what sort of windows aren't going to be able to capture key events? i.e. someone playing a fullscreen videogame?
+
+
 */
 
 const fs = require('fs');
